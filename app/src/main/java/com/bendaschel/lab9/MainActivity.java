c086a39b8ff725cfc3944cfcdcc0179ef979b5fd
Benjamin Daschel <coffeemaxed@gmail.com>
2016-03-10 19:08:44 -0800

    basic working map activity

--- app/src/main/java/com/bendaschel/lab9/MainActivity.java
+++ app/src/main/java/com/bendaschel/lab9/MainActivity.java
@@ -1,13 +1,42 @@
 package com.bendaschel.lab9;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import butterknife.Bind;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private static final String TAG = "MainActivity";
+    private FragmentManager mFragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFragmentManager = getFragmentManager();
+        initMap();
+    }
+
+    private void initMap() {
+        MapFragment mapFragment = MapFragment.newInstance();
+        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, mapFragment);
+        fragmentTransaction.commit();
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady");
     }
 }
